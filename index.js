a98f80ba25ce8ac34cf0b71d769437779e074ac1
Petrut <ivanoiu.petrut27@yahoo.com>
2022-09-25 18:42:17 +0300

    feat: add askAction functionality

--- index.js
+++ index.js
@@ -2,7 +2,64 @@
 
 import chalk from "chalk";
 import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
+// add fs
+import fs from "fs";
 
-console.log(chalk.green("Starting app in dev mode..."));
+let isTimerRunning = false;
 
-const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+console.log(gradient.pastel.multiline("CLI Produciviy"));
+
+// const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function handleAction(action) {
+  switch (action) {
+    case "timer":
+      if (!isTimerRunning) {
+        startTimer();
+      }
+      break;
+    case "stop":
+      stopTimer();
+      break;
+    case "exit":
+      console.log("Goodbye!");
+      process.exit();
+    default:
+      console.log("Invalid action");
+  }
+}
+
+async function askAction() {
+  const { action } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: `Start the timer ${isTimerRunning ? "good" : "bad"}`,
+          value: "timer",
+        },
+        { name: "Stop a timer", value: "stop" },
+        { name: "Add a note", value: "note" },
+        { name: "Exit", value: "exit" },
+      ],
+    },
+  ]);
+  return handleAction(action);
+}
+
+async function startTimer() {
+  isTimerRunning = true;
+}
+
+async function run() {
+  while (true) {
+    await askAction();
+  }
+}
+
+run();
+// await askAction();
